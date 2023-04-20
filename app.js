6c7aa123d54990649524643a95643fcc38df1910
Prashant Saini <prashantsaini.code@gmail.com>
2023-04-20 20:34:28 +0530

    updation in script

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-var query = document.querySelector("#btn-tanslate");
-query.addEventListener("click",function response(){
-      console.log("clicked");
-});
+var btnTranslate = document.querySelector("#btn-tanslate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output-div");
+
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function clickEventHandler() {
+      var serverUrlWithText = serverUrl + "?text=" + txtInput.value;
+
+      fetch(serverUrlWithText)
+      .then(response => response.json())
+      .then(json => {outputDiv.innerText = json.contents.translated});
+      
+};
+
+btnTranslate.addEventListener("click",clickEventHandler);
